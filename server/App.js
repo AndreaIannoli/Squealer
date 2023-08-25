ec9b8f5d0b68ab6b5890397541698088cb943a73
Andrea Iannoli <andrea.iannoli01@gmail.com>
2023-08-25 13:48:11 +0200

    Added DB connection

--- server/App.js
+++ server/App.js
@@ -1,18 +1,42 @@
 const config = require('./Config');
 const express = require('express');
+const mongoose = require('mongoose');
+const userModel = require("./models");
 const app = express ();
 app.use(express.json());
-
 const PORT = config.getPort();
+
+mongoose.connect('mongodb://localhost:27017/Squealer');
+
+app.get('/status', (request, response) => {
+    const status = {
+          'Status': 'Running'
+    };
+
+    response.send(status);
+});
+
+app.post("/add_user", async (request, response) => {
+    const user = new userModel(request.body);
+
+    try {
+        await user.save();
+        response.send(user);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/users", async (request, response) => {
+    const users = await userModel.find({});
+
+    try {
+        response.send(users);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
-});
-
-app.get('/status', (request, response) => {
-const status = {
-      'Status': 'Running'
-};
-
-response.send(status);
 });
